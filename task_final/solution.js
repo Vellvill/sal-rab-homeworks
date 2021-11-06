dd19f4aedadc48446eab5ff865d15af2b7f2cc7f
Timofey <gerasimoff.timofeiigit config --global user.email gerasimoff.timofeii>
2021-11-06 19:07:25 +0300

    done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {goods: [], order: {address,sum}, client: {name,phone}};
 
     let countOfGoods = goods.length;
 
@@ -43,9 +43,9 @@ function sendRequest(name, phone, address, goods, sum) {
     }
 
     data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = name + phone;
 
     let jsonData = JSON.stringify(data);
 
