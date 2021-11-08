01330fa61c54b4bf4902588250e7bbfde4f62d4e
Timofey <gerasimoff.timofeiigit config --global user.email gerasimoff.timofeii>
2021-11-08 20:21:44 +0300

    finally done smert

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,29 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {address,sum}, client: {name,phone}};
+
+
+
+    let data = {client: {name,phone}, order: {address, sum}, goods: []};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    for (let i = 0; i < countOfGoods; i++) {
 
-    data.order.address = address;
+        let title = data.goods.push(goods[i]);
+        delete goods[i].id
+        delete goods[i].price
+        
+    }  
+    
+
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`
+     
     data.order.sum = sum;
 
-    data.client = name + phone;
+    data.client = name + " " + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
